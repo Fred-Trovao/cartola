051a232154fcfaa718bac803d66e977aee369300
Fred Farias <fredmfarias@gmail.com>
2016-05-31 14:10:11 -0300

    ajustes

--- src/main/webapp/js/value/configValue.js
+++ src/main/webapp/js/value/configValue.js
@@ -1,3 +1,3 @@
 app.value("config", {
-	baseUrl: "http://192.168.30.16:8080/cartola/request/forward/https//api.cartolafc.globo.com/"
+	baseUrl: "http://localhost:8080/cartola/request/forward/https//api.cartolafc.globo.com/"
 });
